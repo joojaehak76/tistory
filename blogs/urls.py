c0b4de85a8259fa4771c8c41169ebb0630d3d113
joojaehak76 <joojaehak76@gmail.com>
2022-11-28 19:14:13 +0900

    modify

--- blogs/urls.py
+++ blogs/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.posts_list, name='posts_list'),
     path('post/', views.posts_list, name='posts_list'),
     path('post/<int:post_id>/', views.post_detail, name='post_detail'),
     path('post/write', views.post_write, name='post_write'),
