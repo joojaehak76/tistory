0c63d363452d0d4e0c824bcfc76b12febbc59bd1
joojaehak76 <joojaehak76@gmail.com>
2022-11-28 19:13:17 +0900

    tistory_urls modify

--- tistory/urls.py
+++ tistory/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('users.urls')),
+    path('', include('blogs.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('blogs/', include('blogs.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
